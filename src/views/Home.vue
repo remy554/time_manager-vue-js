a4659fd610304d7c88825c9d57c281cbb31cbaf5
Remy <remy554@yahoo.fr>
2020-11-04 23:00:51 +0100

    Integration test of the ClockManager component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,26 @@
     <h1> Time Manager Home </h1>
     <header class="jumbotron">
       <h3>{{content}}</h3>
+      <!-- <h3> {{ userid }} </h3> -->
+       <ClockManager/>
     </header>
   </div>
 </template>
 
 <script>
 // import UserService from '../services/user-service'
+import ClockManager from '../components/ClockManager.vue'
 
 export default {
   name: 'Home',
   data () {
     return {
-      content: ''
+      content: '',
+      userid: ''
     }
   },
   mounted () {
+    // this.userid = JSON.parse(localStorage.getItem('user')).id
     // UserService.getPublicContent().then(
     //   response => {
     //     this.content = response.data
@@ -29,6 +34,9 @@ export default {
     //       error.toString()
     //   }
     // )
+  },
+  components: {
+    ClockManager
   }
 }
 </script>
