f60f4c290644696b096cf56d3e1a0c2f12de71a0
Remy <remy554@yahoo.fr>
2020-11-05 22:17:05 +0100

    Logout component

---
+++ src/components/Logout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <label>Disconnect from your account</label>
+      <div class="form-group">
+        <button href @click.prevent="logOut" :disabled="loading">
+          <span v-show="loading"></span>
+          <span>Logout</span>
+        </button>
+      </div>
+      <div
+        v-if="message"
+        class="alert"
+        :class="successful ? 'alert-success' : 'alert-danger'"
+      >{{message}}
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'Logout',
+  data () {
+    return {
+      loading: false,
+      message: '',
+      successful: false
+    }
+  },
+  methods: {
+    logOut: function (event) {
+      this.$store.dispatch('signout').then(
+        data => {
+          this.$cookies.remove('jwt')
+          this.message = data.message
+          this.successful = true
+          this.$router.push({ name: 'login' })
+        },
+        error => {
+          console.log('then dispatch logout error')
+          this.message =
+                (error.response && error.response.data && error.response.data.message) ||
+                error.message ||
+                error.toString()
+          this.successful = false
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
