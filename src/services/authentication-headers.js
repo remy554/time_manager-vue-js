99321be17f731260f4f9b6fd2a17012f63a46ae1
Remy <remy554@yahoo.fr>
2020-11-05 14:00:44 +0100

    Workaround for using the auth header (use localStorage instead of cookies temporarily)

--- src/services/authentication-headers.js
+++ src/services/authentication-headers.js
@@ -1,11 +1,13 @@
 /* Authorization headers with the user token */
-import Vue from 'vue'
+// import Vue from 'vue'
+
 export default function authHeader () {
-  // const jwt = JSON.parse(localStorage.getItem('jwt'))
-  const jwt = Vue.$cookies.get('jwt') // can't access this from this scope
+  const jwt = JSON.parse(localStorage.getItem('jwt'))
+  // const jwt = Vue.$cookies.get('jwt') // can't access this from this scope
+  console.log('authHeader jwt : ' + jwt)
 
   if (jwt) {
-    return { Authorization: 'Bearer ' + jwt }
+    return { Authorization: jwt }
   } else {
     return {}
   }
