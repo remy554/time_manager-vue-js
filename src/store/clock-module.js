2cf3e8ddb722ef68201da83c817260106422588c
Remy <remy554@yahoo.fr>
2020-11-06 04:58:39 +0100

    Fixed clocking mutations in vuex store

--- src/store/clock-module.js
+++ src/store/clock-module.js
@@ -4,7 +4,7 @@ import ClockManagerService from '../services/clock-manager-service'
 
 const user = JSON.parse(localStorage.getItem('user'))
 const initialState = user
-  ? { status: { clockedIn: true } }
+  ? { status: { clockedIn: false } }
   : { status: { clockedOut: false } }
 
 export const clock = {
@@ -13,8 +13,18 @@ export const clock = {
     clock ({ commit }) {
       return ClockManagerService.clock().then(
         response => {
-          commit('clockSuccess')
-          return Promise.resolve(response.data)
+          if (response.data.clock) {
+            commit('clockInSuccess')
+            return Promise.resolve(response.data)
+          } else {
+            if (response.data.workingtime) {
+              commit('clockOutSuccess')
+              return Promise.resolve(response.data)
+            } else {
+              // case where the working time is not returned because the clock out can't be validated by the API
+
+            }
+          }
         },
         error => {
           commit('clockFailure')
@@ -24,11 +34,16 @@ export const clock = {
     }
   },
   mutations: {
-    clockSuccess (state) {
+    clockInSuccess (state) {
       state.status.clockedIn = true
+      state.status.clockedOut = false
+    },
+    clockOutSuccess (state) {
+      state.status.clockedOut = true
+      state.status.clockedIn = false
     },
     clockFailure (state) {
-      state.status.clockedIn = false
+      // change nothing
     }
   }
 }
